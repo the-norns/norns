6a79b063558735b0739a99781e582d7ce3e6ce98
Dave Snowberger <git@fartherout.org>
2018-05-21 11:45:21 -0700

    fixed model bugs and made migrations

--- norns/gear/models.py
+++ norns/gear/models.py
@@ -4,7 +4,7 @@ from room.models import Tile
 
 class Weapon(models.Model):
     """Weapon model."""
-    name = models.CharField()
+    name = models.CharField(max_length=255)
     strength = models.IntegerField()
     agility = models.IntegerField()
     tiles = models.ManyToManyField(Tile, related_name='weapons', blank=True)
--- norns/room/models.py
+++ norns/room/models.py
@@ -3,14 +3,34 @@ from django.db import models
 
 class Room(models.Model):
     """Room model."""
-    room_north = models.ForeignKey('self', blank=True)
-    room_east = models.ForeignKey('self', blank=True)
-    room_south = models.ForeignKey('self', blank=True)
-    room_west = models.ForeignKey('self', blank=True)
+    room_north = models.ForeignKey(
+        'self',
+        blank=True,
+        related_name='to_south',
+        on_delete=models.CASCADE,
+    )
+    room_east = models.ForeignKey(
+        'self',
+        blank=True,
+        related_name='to_west',
+        on_delete=models.CASCADE,
+    )
+    room_south = models.ForeignKey(
+        'self',
+        blank=True,
+        related_name='to_north',
+        on_delete=models.CASCADE,
+    )
+    room_west = models.ForeignKey(
+        'self',
+        blank=True,
+        related_name='to_east',
+        on_delete=models.CASCADE,
+    )
 
 
 class Tile(models.Model):
     """Tile model."""
-    room = models.ForeignKey(Room)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     x_coord = models.IntegerField()
     y_coord = models.IntegerField()

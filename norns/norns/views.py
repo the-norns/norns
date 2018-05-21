1b5c8de186647baaed2493f051c640f9dffbdd77
Kat Cosgrove <kat.cosgrove@gmail.com>
2018-05-21 14:58:43 -0700

    barebones stripe operational

--- norns/norns/views.py
+++ norns/norns/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+import stripe
 
 
 def home_view(request):
@@ -6,9 +9,29 @@ def home_view(request):
     return render(request, 'home.html')
 
 
-def store_view(request):
-    """Return store view."""
-    return render(request, 'store.html')
+class StoreView(TemplateView):
+    """Return store view and handle payments."""
+
+    template_name = 'store.html'
+
+    def get_context_data(self, **kwargs):
+        """Get request for homepage class view."""
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def post(self, *args, **kwargs):
+        """Handle post request for order form."""
+        stripe.api_key = "sk_test_bDdZgeBXWJOYecXwD7W2ta0n"
+
+        token = self.request.POST['stripeToken']
+
+        charge = stripe.Charge.create(
+            amount=2000,
+            currency='usd',
+            description='Example charge',
+            source=token,
+        )
+        return render(self.request, 'store.html', {'message': 'Thanks for your money, sucker!'})
 
 
 def about_view(request):

adbd89732cc17adbd85174f634a7ad7cbdc015f6
Kat Cosgrove <kat.cosgrove@gmail.com>
2018-05-20 20:24:16 -0700

    wireframes in place, super basic views in place, css needs linking

---
+++ norns/norns/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def home_view(request):
+    """Return home view."""
+    return render(request, 'home.html')
+
+
+def store_view(request):
+    """Return store view."""
+    return render(request, 'store.html')
+
+
+def about_view(request):
+    """Return about view."""
+    return render(request, 'about.html')

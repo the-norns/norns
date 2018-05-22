6262b153a504a700b3057fa3195fd99ff35a2296
Dave Snowberger <git@fartherout.org>
2018-05-22 11:05:39 -0700

    moved methods to models, updated models

--- norns/enemy/models.py
+++ norns/enemy/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from gear.models import Weapon
+from gear.models import Weapon, Inventory
 from room.models import Tile
 from status.models import Ability
 
@@ -11,8 +11,15 @@ class Enemy(models.Model):
     """
 
     name = models.CharField(max_length=255, default='Unnamed')
+    health = models.IntegerField(default=10)
     abilities = models.ManyToManyField(Ability, blank=True)
-    loot = models.ForeignKey(
+    inventory = models.ForeignKey(
+        Inventory,
+        blank=True,
+        related_name='enemies',
+        on_delete=models.CASCADE,
+        null=True)
+    weapon = models.ForeignKey(
         Weapon,
         blank=True,
         related_name='enemies',
--- norns/room/views.py
+++ norns/room/views.py
@@ -1,15 +1,11 @@
-from random import randint
-
-from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from rest_framework.generics import (
     CreateAPIView, RetrieveAPIView, UpdateAPIView)
-
-from enemy.models import Enemy
+from django.db.models import Q
 from gear.models import Weapon
 from player.models import Player
 
-from .models import Room, Tile
+from .models import Room
 from .serializers import RoomSerializer
 
 
@@ -19,109 +15,6 @@ class RoomView(CreateAPIView, RetrieveAPIView, UpdateAPIView):
     """
 
     serializer_class = RoomSerializer
-    grid_size = 5
-
-    @staticmethod
-    def roll_tile(tile):
-        """
-        Create disapointment.
-        """
-        roll = randint(0, 10)
-        if roll < 2:
-            Weapon.order_by('?').first().tiles.add(tile)
-
-    def roll_room(self, direction, prev_room):
-        """
-        Generate room.
-        """
-        room = Room.create()
-        if direction == 'north':
-            room.room_south = prev_room
-            prev_room.room_north = room
-        if direction == 'east':
-            room.room_west = prev_room
-            prev_room.room_east = room
-        if direction == 'south':
-            room.room_north = prev_room
-            prev_room.room_south = room
-        if direction == 'west':
-            room.room_east = prev_room
-            prev_room.room_west = room
-        room.save()
-
-        for x in range(self.grid_size):
-            for y in range(self.grid_size):
-                tile = Tile.create(x_coord=x, y_coord=y, room=room)
-                roll = randint(0, 10)
-                if roll == 1:
-                    Enemy.order_by('?').first().tiles.add(tile)
-
-        return room
-
-    def move_player(self, player, direction):
-        """
-        Change player tile.
-        """
-        if direction == 'north':
-            queryset = Tile.objects.filter(
-                Q(y_coord=player.tile.y_coord - 1) &
-                Q(x_coord=player.tile.x_coord) &
-                Q(room=player.tile.room))
-            if queryset.count():
-                player.tile = queryset.first()
-            else:
-                room = player.tile.room.room_north
-                if not room:
-                    room = self.roll_room(direction, player.tile.room)
-                player.tile = Tile.objects.filter(
-                    Q(y_coord=self.grid_size - 1) &
-                    Q(x_coord=player.tile.x_coord) &
-                    Q(room=room))
-        elif direction == 'east':
-            queryset = Tile.objects.filter(
-                Q(y_coord=player.tile.y_coord) &
-                Q(x_coord=player.tile.x_coord + 1) &
-                Q(room=player.tile.room))
-            if queryset.count():
-                player.tile = queryset.first()
-            else:
-                room = player.tile.room.room_east
-                if not room:
-                    room = self.roll_room(direction, player.tile.room)
-                player.tile = Tile.objects.filter(
-                    Q(y_coord=player.tile.y_coord) &
-                    Q(x_coord=0) &
-                    Q(room=room))
-        elif direction == 'south':
-            queryset = Tile.objects.filter(
-                Q(y_coord=player.tile.y_coord + 1) &
-                Q(x_coord=player.tile.x_coord) &
-                Q(room=player.tile.room))
-            if queryset.count():
-                player.tile = queryset.first()
-            else:
-                room = player.tile.room.room_south
-                if not room:
-                    room = self.roll_room(direction, player.tile.room)
-                player.tile = Tile.objects.filter(
-                    Q(y_coord=0) &
-                    Q(x_coord=player.tile.x_coord) &
-                    Q(room=room))
-        elif direction == 'west':
-            queryset = Tile.objects.filter(
-                Q(y_coord=player.tile.y_coord) &
-                Q(x_coord=player.tile.x_coord - 1) &
-                Q(room=player.tile.room))
-            if queryset.count():
-                player.tile = queryset.first()
-            else:
-                room = player.tile.room.room_west
-                if not room:
-                    room = self.roll_room(direction, player.tile.room)
-                player.tile = Tile.objects.filter(
-                    Q(y_coord=player.tile.y_coord) &
-                    Q(x_coord=self.grid_size - 1) &
-                    Q(room=room))
 
     def get_queryset(self):
         """
@@ -135,14 +28,17 @@ class RoomView(CreateAPIView, RetrieveAPIView, UpdateAPIView):
         """
         player = get_object_or_404(Player, user=request.user)
         user_input = self.request.data['user_input'].split()
-        if 'pk' in self.kwargs:
-            if user_input[0] == 'look':
-                if not player.tile.looked:
-                    self.roll_tile(player.tile)
-                return super().retrieve(request, self.kwargs['pk'])
+        verb = user_input[0]
+        if verb == 'look':
+            if not player.tile.looked:
+                player.tile.roll_tile()
+            return super().retrieve(request, self.kwargs['pk'])
+        if verb == 'take':
+            weapon = Weapon.objects.filter(
+                Q(tiles=player.tile) &
+                Q(name=user_input[1])).first()
+            if weapon:
+                player.inventory.weapons.add(weapon)
+                weapon.tiles.remove(player.tile)
 
-            if user_input[0] == 'go':
-                self.move_player(player, user_input[1])
-
-        else:
-            return self.roll_room(user_input[1], self.kwargs['prev_room_id'])
+        player.handle_user_input(user_input)

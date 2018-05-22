e062062ffd3b03cc14e795d716f3ef0391ac499d
Kat Cosgrove <kat.cosgrove@gmail.com>
2018-05-22 11:13:00 -0700

    serializer and view added for enemies; getting fixtures in place for preloaded data

---
+++ norns/enemy/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import Enemy
+
+
+class EnemySerializer(serializers.ModelSerializer):
+    """Serializer for enemy model."""
+
+    class Meta:
+        """Meta class for model fields."""
+
+        model = Enemy
+        fields = ('name', 'abilities', 'loot')

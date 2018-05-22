00f405bcef3319909ab8a1ce85a2e18878336654
Adam Grandquist <grandquista@gmail.com>
2018-05-22 13:32:06 -0700

    Add consumable model.

--- norns/gear/models.py
+++ norns/gear/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from room.models import Tile
-
 
 class Weapon(models.Model):
     """
@@ -11,9 +9,9 @@ class Weapon(models.Model):
     name = models.CharField(max_length=255, default='Untitled')
     strength = models.IntegerField(default=0)
     agility = models.IntegerField(default=0)
+    ability = models.ForeignKey(
+        'status.Ability', on_delete=models.SET_NULL, null=True)
     reach = models.IntegerField(default=0)
-    tiles = models.ManyToManyField(
-        Tile, related_name='weapons', blank=True)
 
 #    def attack(self, enemy):
 #        """
@@ -21,10 +19,34 @@ class Weapon(models.Model):
 #        """
 
 
+class Consumable(models.Model):
+    """
+    Consumable model.
+    """
+
+    name = models.CharField(max_length=255, default='Untitled')
+    ability = models.ForeignKey(
+        'status.Ability', on_delete=models.SET_NULL, null=True)
+
+    def loot(self, player):
+        """
+        Loot consumable.
+        """
+        self.tiles.remove(player.tile)
+        player.consumables.add(self)
+
+    def handle_use(self, player, target):
+        """
+        Use consumable.
+        """
+        player.consumables.remove(self)
+        self.ability.use_ability(player, target)
+
+
 class Inventory(models.Model):
     """
     Inventory model.
     """
 
-    weapons = models.ManyToManyField(
-        Weapon, related_name='inventories', blank=True)
+    weapons = models.ManyToManyField(Weapon, blank=True)
+    consumables = models.ManyToManyField(Consumable, blank=True)

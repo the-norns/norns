fd7e1cc89d26bcdb2a2629166f5f0371c79080ea
Kat Cosgrove <kat.cosgrove@gmail.com>
2018-05-22 11:13:00 -0700

    serializer and view added for enemies; getting fixtures in place for preloaded data

--- norns/enemy/views.py
+++ norns/enemy/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Enemy
+from .serializers import EnemySerializer
+
+
+class WeaponView(generics.CreateAPIView, generics.RetrieveAPIView,
+                 generics.UpdateAPIView):
+    """Return JSON of enemy."""
+
+    serializer_class = EnemySerializer
+
+    def get_object(self):
+        """Get an enemy object."""
+        return Enemy.objects.get(id=self.kwargs['pk'])

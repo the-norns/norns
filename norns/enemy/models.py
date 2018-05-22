164b5db3d0ad42f9ff586e52f0c39bae74861cf8
Dave Snowberger <git@fartherout.org>
2018-05-21 18:34:14 -0700

    updated models to take care of 500 errors

--- norns/enemy/models.py
+++ norns/enemy/models.py
@@ -6,7 +6,9 @@ from room.models import Tile
 
 class Enemy(models.Model):
     """Enemy model."""
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, default='Unnamed')
     abilities = models.ManyToManyField(Ability, blank=True)
-    loot = models.ForeignKey(Weapon, blank=True, on_delete=models.CASCADE)
-    tiles = models.ManyToManyField(Tile, related_name='enemies', blank=True)
+    loot = models.ForeignKey(
+        Weapon, blank=True, on_delete=models.CASCADE, null=True)
+    tiles = models.ManyToManyField(
+        Tile, related_name='enemies', blank=True)
--- norns/status/models.py
+++ norns/status/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 
 class Ability(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, blank=True, null=True)

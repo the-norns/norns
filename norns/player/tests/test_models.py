cb86b43b8f3e2948468e76d0ea65dfe322d609c2
Asa Katida <github@holomaplefeline.net>
2018-05-23 08:16:18 -0700

    Test room creation continuity.

--- norns/player/tests/test_models.py
+++ norns/player/tests/test_models.py
@@ -138,6 +138,96 @@ class TestModels(TestCase):
         self.assertNotEqual(start, end)
 
 
+class TestRoomContinuity(TestCase):
+    """
+    Test Player model.
+    """
+
+    def setUp(self):
+        """
+        Create player models.
+        """
+        self.user = mommy.make(User)
+
+    def tearDown(self):
+        """
+        Destroy player models.
+        """
+        Player.objects.all().delete()
+
+    def test_player_can_move_north_south(self):
+        """
+        Validate player moves north south.
+        """
+        player = Player.objects.filter(user=self.user).first()
+        start = player.tile
+        self.assertIsNotNone(start)
+        while player.tile.room == start.room:
+            player.move('north')
+        end = player.tile
+        self.assertIsNotNone(end)
+        self.assertNotEqual(start, end)
+        player.move('south')
+        self.assertEqual(player.tile.room, start.room)
+        while player.tile.room == start.room:
+            player.move('south')
+        self.assertIsNotNone(player.tile.room)
+
+    def test_player_can_move_south_north(self):
+        """
+        Validate player moves south north.
+        """
+        player = Player.objects.filter(user=self.user).first()
+        start = player.tile
+        self.assertIsNotNone(start)
+        while player.tile.room == start.room:
+            player.move('south')
+        end = player.tile
+        self.assertIsNotNone(end)
+        self.assertNotEqual(start, end)
+        player.move('north')
+        self.assertEqual(player.tile.room, start.room)
+        while player.tile.room == start.room:
+            player.move('north')
+        self.assertIsNotNone(player.tile.room)
+
+    def test_player_can_move_east_west(self):
+        """
+        Validate player moves east west.
+        """
+        player = Player.objects.filter(user=self.user).first()
+        start = player.tile
+        self.assertIsNotNone(start)
+        while player.tile.room == start.room:
+            player.move('east')
+        end = player.tile
+        self.assertIsNotNone(end)
+        self.assertNotEqual(start, end)
+        player.move('west')
+        self.assertEqual(player.tile.room, start.room)
+        while player.tile.room == start.room:
+            player.move('west')
+        self.assertIsNotNone(player.tile.room)
+
+    def test_player_can_move_west_east(self):
+        """
+        Validate player moves west east.
+        """
+        player = Player.objects.filter(user=self.user).first()
+        start = player.tile
+        self.assertIsNotNone(start)
+        while player.tile.room == start.room:
+            player.move('west')
+        end = player.tile
+        self.assertIsNotNone(end)
+        self.assertNotEqual(start, end)
+        player.move('east')
+        self.assertEqual(player.tile.room, start.room)
+        while player.tile.room == start.room:
+            player.move('east')
+        self.assertIsNotNone(player.tile.room)
+
+
 class TestModelsWithData(TestCase):
     """
     Test Player model.

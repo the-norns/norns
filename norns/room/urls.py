4a59450ed9d8a2f9d5c9b5fad15be84db41ca394
Dave Snowberger <git@fartherout.org>
2018-05-23 08:29:25 -0700

    tile serializer and endpoint added.

--- norns/room/urls.py
+++ norns/room/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import RoomView, NewRoomView
+from .views import RoomView, NewRoomView, TileView
 
 urlpatterns = [
     path('', RoomView.as_view(), name='room'),
-    path('new', NewRoomView.as_view(), name='new_room')
+    path('new', NewRoomView.as_view(), name='new_room'),
+    path('tile/<int:pk>', TileView.as_view(), name='tile'),
 ]

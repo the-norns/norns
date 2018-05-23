837f5eac6b10d91a697478f35bed617102f6ed5f
Kat Cosgrove <kat.cosgrove@gmail.com>
2018-05-23 11:41:02 -0700

    render of tiles works, but spacing needs major adjustment

--- norns/norns/static/js/norns.js
+++ norns/norns/static/js/norns.js
@@ -1,59 +1,34 @@
 let CANVAS_WIDTH = 1000;
 let CANVAS_HEIGHT = 470;
-let tileX = 50;
-let tileY = 50;
-const FPS = 60;
+let roomTiles = [];
 const __API_URL__ = 'http://localhost:8000/api/v1/'
-// var tileRender = {
-//   color: "#00A",
-//   x: 0,
-//   y: 0,
-//   width: 32,
-//   height: 32,
-//   draw: function() {
-//     canvas.fillStyle = this.color;
-//     canvas.fillRect(this.x, this.y, this.width, this.height);
-//   }
-// };
-
-function Tile(x, y) {
-    this.x = x;
-    this.y = y;
-    this.width = 32;
-    this.height = 32;
-    this.color = "#00A";
-    this.draw = function() {
-    canvas.fillStyle = this.color;
-    canvas.fillRect(this.x, this.y, this.width, this.height);
-  }
-}
 
 let canvasElement = $("<canvas width='" + CANVAS_WIDTH + 
                       "' height='" + CANVAS_HEIGHT + "'></canvas>");
 let canvas = canvasElement.get(0).getContext("2d");
+
 canvasElement.appendTo($(".game"));
 
-setInterval(function() {
-  update();
-  draw();
-}, 1000/FPS);
-
-function update() {
-  tileX += 1;
-  tileY += 1;
-}
-
-function draw() {
-  canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  canvas.fillStyle = "#000";
-  canvas.fillText("Hello World", tileX, tileY);
+function Tile(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.draw = function() {
+    canvas.fillStyle = this.color;
+    canvas.fillRect(this.x * 10, this.y * 10, 5, 5);
+  }
 }
 
 $.post(`${__API_URL__}room/new`, function (data) {
-    // console.log(data)
     data.tiles.forEach(function(tile){
-        testTile = new Tile(tile.x_coord, tile.y_coord, "#00A")
-        console.log(testTile)
-        testTile.draw()
+        roomTiles.push(new Tile(tile.x_coord, tile.y_coord, "#00A"))
     })
 })
+.then(console.log(roomTiles))
+.then(() => draw(roomTiles))
+
+
+function draw(tiles) {
+  // canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  tiles.forEach(function(tile){tile.draw()})
+}

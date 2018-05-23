3d00e922640b074811804685398de77eddc25ccc
Kat Cosgrove <kat.cosgrove@gmail.com>
2018-05-23 09:36:16 -0700

    canvas in place, building game board

---
+++ norns/norns/static/js/norns.js
@@ -0,0 +1,26 @@
+let CANVAS_WIDTH = 480;
+let CANVAS_HEIGHT = 320;
+let tileX = 50;
+let tileY = 50;
+const FPS = 60;
+
+let canvasElement = $("<canvas width='" + CANVAS_WIDTH + 
+                      "' height='" + CANVAS_HEIGHT + "'></canvas>");
+let canvas = canvasElement.get(0).getContext("2d");
+canvasElement.appendTo($(".game"));
+
+setInterval(function() {
+  update();
+  draw();
+}, 1000/FPS);
+
+function update() {
+  tileX += 1;
+  tileY += 1;
+}
+
+function draw() {
+  canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  canvas.fillStyle = "#000";
+  canvas.fillText("Hello World", tileX, tileY);
+}

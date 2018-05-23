37df5325b89c96f66d4ce437b91905a3cfe967c6
Adam Grandquist <grandquista@gmail.com>
2018-05-23 08:25:03 -0700

    Test validity of view code.

--- norns/enemy/tests/test_views.py
+++ norns/enemy/tests/test_views.py
@@ -1,7 +1,15 @@
 from django.test import TestCase
 
+from ..views import WeaponView
+
 
 class TestViews(TestCase):
     """
     Test views.
     """
+
+    def test_valid_view(self):
+        """
+        Use import.
+        """
+        self.assertIsNotNone(WeaponView.as_view())

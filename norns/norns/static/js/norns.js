efe615cec8a2934069d0bb88c78bd25ce2a580fb
Kat Cosgrove <kat.cosgrove@gmail.com>
2018-05-23 10:36:21 -0700

    getting tile data, still working on dynamic render

--- norns/norns/static/js/norns.js
+++ norns/norns/static/js/norns.js
@@ -3,6 +3,30 @@ let CANVAS_HEIGHT = 470;
 let tileX = 50;
 let tileY = 50;
 const FPS = 60;
+const __API_URL__ = 'http://localhost:8000/api/v1/'
+// var tileRender = {
+//   color: "#00A",
+//   x: 0,
+//   y: 0,
+//   width: 32,
+//   height: 32,
+//   draw: function() {
+//     canvas.fillStyle = this.color;
+//     canvas.fillRect(this.x, this.y, this.width, this.height);
+//   }
+// };
+
+function Tile(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 32;
+    this.height = 32;
+    this.color = "#00A";
+    this.draw = function() {
+    canvas.fillStyle = this.color;
+    canvas.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
 
 let canvasElement = $("<canvas width='" + CANVAS_WIDTH + 
                       "' height='" + CANVAS_HEIGHT + "'></canvas>");
@@ -24,3 +48,12 @@ function draw() {
   canvas.fillStyle = "#000";
   canvas.fillText("Hello World", tileX, tileY);
 }
+
+$.post(`${__API_URL__}room/new`, function (data) {
+    // console.log(data)
+    data.tiles.forEach(function(tile){
+        testTile = new Tile(tile.x_coord, tile.y_coord, "#00A")
+        console.log(testTile)
+        testTile.draw()
+    })
+})

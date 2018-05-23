ed6d87a0646762ca05ee80938ac0f1d0eb9968ad
Asa Katida <github@holomaplefeline.net>
2018-05-23 08:03:58 -0700

    Test equiping weapon.

--- norns/player/tests/test_models.py
+++ norns/player/tests/test_models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from model_mommy import mommy
 
+from gear.models import Weapon
 from ..models import Player
 
 
@@ -135,3 +136,76 @@ class TestModels(TestCase):
         end = player.tile
         self.assertIsNotNone(end)
         self.assertNotEqual(start, end)
+
+
+class TestModelsWithData(TestCase):
+    """
+    Test Player model.
+    """
+
+    fixtures = ['fixture']
+
+    def setUp(self):
+        """
+        Create player models.
+        """
+        self.user = mommy.make(User)
+
+    def tearDown(self):
+        """
+        Destroy player models.
+        """
+        Player.objects.all().delete()
+
+    def test_player_action_go(self):
+        """
+        Validate player moves.
+        """
+        player = Player.objects.filter(user=self.user).first()
+        start = player.tile
+        self.assertIsNotNone(start)
+        player.handle_user_input('go north'.split())
+        self.assertNotEqual(start, player.tile)
+        player.handle_user_input('go east'.split())
+        self.assertNotEqual(start, player.tile)
+        player.handle_user_input('go south'.split())
+        self.assertNotEqual(start, player.tile)
+        player.handle_user_input('go west'.split())
+        end = player.tile
+        self.assertIsNotNone(end)
+        self.assertEqual(start, end)
+
+
+class TestPlayerWithInventory(TestCase):
+    """
+    Test Player model.
+    """
+
+    fixtures = ['fixture']
+
+    def setUp(self):
+        """
+        Create player models.
+        """
+        self.user = mommy.make(User)
+        player = Player.objects.filter(user=self.user).first()
+        for _ in range(10):
+            weapon = mommy.make(Weapon)
+            player.inventory.weapons.add(weapon)
+
+    def tearDown(self):
+        """
+        Destroy player models.
+        """
+        Player.objects.all().delete()
+
+    def test_player_action_equip(self):
+        """
+        Validate player moves.
+        """
+        player = Player.objects.filter(user=self.user).first()
+        weapon = player.weapon
+        self.assertIsNone(weapon)
+        player.handle_user_input(
+            'equip {}'.format(
+                player.inventory.weapons.first().name).split())

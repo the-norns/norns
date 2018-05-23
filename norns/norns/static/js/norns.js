4f1f5e5116b342a4a68a4e52fff6030216691516
Kat Cosgrove <kat.cosgrove@gmail.com>
2018-05-23 12:27:51 -0700

    new game triggered by button press, board renders

--- norns/norns/static/js/norns.js
+++ norns/norns/static/js/norns.js
@@ -1,4 +1,4 @@
-let CANVAS_WIDTH = 1000;
+let CANVAS_WIDTH = 800;
 let CANVAS_HEIGHT = 470;
 let roomTiles = [];
 const __API_URL__ = 'http://localhost:8000/api/v1/'
@@ -9,26 +9,35 @@ let canvas = canvasElement.get(0).getContext("2d");
 
 canvasElement.appendTo($(".game"));
 
-function Tile(x, y, color) {
+function Tile(x, y, consumables, enemies, players, weapons) {
     this.x = x;
     this.y = y;
-    this.color = color;
+    this.color = "#00A";
+    this.enemies = enemies;
+    this.players = players;
+    this.weapons = weapons;
     this.draw = function() {
     canvas.fillStyle = this.color;
-    canvas.fillRect(this.x * 10, this.y * 10, 5, 5);
+    canvas.fillRect(this.x * 100, this.y * 100, 99, 99);
   }
 }
 
-$.post(`${__API_URL__}room/new`, function (data) {
-    data.tiles.forEach(function(tile){
-        roomTiles.push(new Tile(tile.x_coord, tile.y_coord, "#00A"))
-    })
-})
-.then(console.log(roomTiles))
-.then(() => draw(roomTiles))
-
-
 function draw(tiles) {
-  // canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   tiles.forEach(function(tile){tile.draw()})
 }
+
+function newGame() {
+    $.post(`${__API_URL__}room/new`, function (data) {
+    data.tiles.forEach(function(tile){
+        roomTiles.push(new Tile(tile.x_coord, tile.y_coord, tile.consumables, tile.enemy_set, tile.player_set, tile.weapons))
+    })
+  })
+    .then(console.log(roomTiles))
+    .then(() => draw(roomTiles))
+}
+
+function clearCanvas() {
+    canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+$(".start-game").on( "click", newGame );

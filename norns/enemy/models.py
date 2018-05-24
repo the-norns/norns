6051710b2134b87d1716daca516252e2c524e9f3
Dave Snowberger <git@fartherout.org>
2018-05-24 16:20:51 -0700

    enemys track players

--- norns/enemy/models.py
+++ norns/enemy/models.py
@@ -98,31 +98,38 @@ class Enemy(models.Model):
         message = ''
         attacked = False
         if self.weapon:
-            for player in self.tile.player_set.all():
-                if self.weapon.check_reach(self, player):
+            for tile in self.tile.room.tile_set.all():
+                for player in tile.player_set.all():
                     message = self.weapon.attack(self, player)
-                    if not self.tile.player_set.count():
-                        self.tile.room.round_start = None
-                    attacked = True
-                    break
+                    if message.split()[-1] != 'range.':
+                        if not self.tile.player_set.count():
+                            self.tile.room.round_start = None
+                        attacked = True
+                        break
 
             if not attacked and not self.tile.player_set.count():
                 dist = 5
-                for player in self.tile.player_set.all():
-                    dx = self.tile.x_coord - player.tile.x_coord
-                    dy = self.tile.y_coord - player.tile.y_coord
-                    new_dist = abs(dx) + abs(dy) / 2
-                    if new_dist < dist:
-                        if abs(dx) > abs(dy):
-                            if dx > 0:
-                                self.move('west')
-                            else:
-                                self.move('east')
-                        else:
-                            if dy > 0:
-                                self.move('north')
-                            else:
-                                self.move('south')
+                min_dx = 5
+                min_dy = 5
+                for tile in self.tile.room.tile_set.all():
+                    for player in tile.player_set.all():
+                        dx = self.tile.x_coord - player.tile.x_coord
+                        dy = self.tile.y_coord - player.tile.y_coord
+                        new_dist = abs(dx) + abs(dy) / 2
+                        if new_dist < dist:
+                            dist = new_dist
+                            min_dx = dx
+                            min_dy = dy
+                if abs(min_dx) > abs(min_dy):
+                    if min_dx > 0:
+                        self.move('west')
+                    else:
+                        self.move('east')
+                else:
+                    if min_dy > 0:
+                        self.move('north')
+                    else:
+                        self.move('south')
         return message
 
 

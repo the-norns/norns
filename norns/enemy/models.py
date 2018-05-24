ed0bedcf3d0c8de7038aef629b1470dfb4147036
Asa Katida <github@holomaplefeline.net>
2018-05-23 17:14:33 -0700

    Constrain inventory.

--- norns/enemy/models.py
+++ norns/enemy/models.py
@@ -20,15 +20,12 @@ class Enemy(models.Model):
     name = models.CharField(max_length=255, default='Unnamed')
     health = models.IntegerField(default=10)
 
-    enemy_type = models.ForeignKey(
-        EnemyType, on_delete=models.CASCADE, null=True)
+    enemy_type = models.ForeignKey(EnemyType, on_delete=models.CASCADE)
 
     abilities = models.ManyToManyField('status.Ability', blank=True)
     inventory = models.OneToOneField(
         'gear.Inventory',
-        blank=True,
-        on_delete=models.CASCADE,
-        null=True)
+        on_delete=models.CASCADE)
     weapon = models.ForeignKey(
         'gear.Weapon',
         blank=True,
@@ -59,6 +56,13 @@ def populate_enemies(sender, created=False, instance=None, **kwargs):
         roll = randint(0, 10)
         if roll > 2:
             return
-        Enemy(
-            enemy_type=EnemyType.objects.order_by('?').first(),
-            tile=instance).save()
+        Enemy.objects.create(tile=instance)
+
+
+@receiver(models.signals.pre_save, sender=Enemy)
+def populate_enemy_type(sender, instance=None, **kwargs):
+    """
+    Generate tile mobs.
+    """
+    if not instance.enemy_type:
+        instance.enemy_type = EnemyType.objects.order_by('?').first()
